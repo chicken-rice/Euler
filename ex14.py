cf26122b469ff0da9106545a49ad1b801527c3a2
Hiroki GOHARA <qi0914@gmail.com>
2013-05-22 23:43:46 +0900

    temp

--- ex14.py
+++ ex14.py
@@ -1,29 +1,24 @@
+def calc_chain(num, data = [-1 for i in xrange(10**8)]):
+
+    if data[num] != -1:
+        return data[num]
+
+    if num == 1:
+        data[1] = 1
+    elif num % 2 == 0:
+        data[num] = calc_chain(num/2) + 1
+    else:
+        data[num] = calc_chain(3*num+1) + 1
+       
+    return data[num] 
+
 if __name__ == '__main__':
-    max_num = 0
-    col_num = 8
-    last_num = 0
-    flag_end = False
+    chain_num = [0 for i in xrange(10**6)]
 
+    for i in xrange(1, 10**6):
+        chain_num[i] = calc_chain(i)
+        #print chain_num[i]
+        #if i > 30:
+        #    break
 
-    count = 0
-
-    while(True):
-        print col_num
-        count += 1
-        if count == 200:
-            break
-        if (col_num-1)%3 == 0\
-        and (col_num-1)/3%3 != 0\
-        and (col_num-1)/3%2 != 0:
-          #  last_num = col_num
-            col_num = (col_num-1) / 3
-            if col_num >= 100**6:
-                break
-        else:
-         #   last_num = col_num
-            col_num *= 2
-
-        if col_num < 10**6 and max_num < col_num:
-                max_num = col_num
-
-    print max_num 
+    print max(xrange(len(chain_num)), key=lambda i: chain_num[i])
