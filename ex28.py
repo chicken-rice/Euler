1f720620e63726b77d958271a348988f35636081
Hiroki GOHARA <qi0914@gmail.com>
2013-05-27 02:38:20 +0900

    ex28

--- ex28.py
+++ ex28.py
@@ -1,9 +1,5 @@
-def diagonal(num):
-    vert_sum = lambda x: 2 * (2*(2*x-1)**2 - 6*(x-1))
-    sum_num = 1
-    sum_num +=  sum(map(vert_sum, xrange(2, (num+3)/2)))
-
-    return sum_num
-
 if __name__ == '__main__':
+    vert_sum = lambda x: 2 * (2*(2*x-1)**2 - 6*(x-1))
+    diagonal = lambda num: 1 + sum(map(vert_sum, xrange(2, (num+3)/2)))
+
     print diagonal(1001)
